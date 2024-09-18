c76128b83bb227d7e92232577d8bf08b68eb7262
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-09-18 10:46:50 -0300

    feature: criação de renderização das listas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import './App.css'
+import List from './components/List';
 
 function App() {
+
+  const [lists, setLists] = useState([])
 
   return (
     <>
       <h1>Gerenciador de Listas</h1>
+      {!lists.length ? 
+        (<h2>Não há listas...</h2>) 
+        : (<div>{lists.map(list => <List title={list.title} items={list.items}></List>)}</div>)
+      }
     </>
   )
 }
