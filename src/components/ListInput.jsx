c4164a037b4055bb724286552533e9b2d62752d2
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-09-18 11:13:43 -0300

    feature: criação inicial do componente de adição de lista

--- src/components/ListInput.jsx
+++ src/components/ListInput.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ListInput = () => {
-  return (
-    <div>ListInput</div>
-  )
+const ListInput = ({addList}) => {
+
+    const [title, setTitle] = useState("")
+
+    const handleAddList = (e) => {
+        e.preventDefault()
+        addList({
+            id: Date.now(),
+            title
+        })
+    }
+
+    return (
+        <form onSubmit={handleAddList}>
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+            <button type="submit">Adicionar Lista</button>
+        </form>
+  );
+
 }
-
 export default ListInput
